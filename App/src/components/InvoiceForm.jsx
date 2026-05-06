3af86cceb6e33653dc57b39867006cbf7ef7c38a
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-07 01:56:49 +0300

    feat: implement form handling in InvoiceForm component

--- App/src/components/InvoiceForm.jsx
+++ App/src/components/InvoiceForm.jsx
@@ -3,11 +3,26 @@ import { useActionState } from "react";
 
 async function actionFunction(previousState, formData) {
   const Clientname = formData.get("name");
-  
+  const date = formData.get("date");
+  const price = formData.get("price");
+
+  return {
+    message: "Form submitted",
+    success: true,
+  };
 }
 
 function InvoiceForm() {
-  return <div>fdas</div>;
+  const [state, formAction] = useActionState(actionFunction, null);
+
+  return (
+    <>
+      <form action={formAction}>
+        <input type="text" />
+        <input type="number" />
+      </form>
+    </>
+  );
 }
 
 export default InvoiceForm;
